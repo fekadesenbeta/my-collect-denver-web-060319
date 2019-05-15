57cb506984488c67979764270a1d3f505ae41fa0
Fekade Senbeta <fekadesenbeta@gmail.com>
2019-05-15 21:12:50 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,3 +1,15 @@
 def my_collect(empty_array)
+  i = []
 end
 
+
+
+
+def my_each(array_collection)
+  i = 0 
+  while i < array_collection.length
+  yield(array_collection[i])
+  i += 1
+  end
+array_collection
+end 	
