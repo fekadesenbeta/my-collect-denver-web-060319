435f384decec7fb2ffa60cec6b882d683766d795
Fekade Senbeta <fekadesenbeta@gmail.com>
2019-05-15 22:25:55 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,10 +1,11 @@
-def my_collect(array)
-  i = 0
-  collection = []
-  while i < array.length
-   collection << yield(array[i])
-   i += 1 
- end
- array
-end
 
+
+def my_collect(collection)
+  i = 0
+  new_collection = []
+  while i < collection.length
+    new_collection << yield(collection[i])
+    i += 1
+  end
+  collection
+end
