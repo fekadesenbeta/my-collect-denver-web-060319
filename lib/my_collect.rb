e4c7a40771a1449ab42b57c0ddbf9eb454bd8502
Fekade Senbeta <fekadesenbeta@gmail.com>
2019-05-15 22:20:41 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,9 +2,9 @@ def my_collect(array)
   i = 0
   collection = []
   while i < array.length
-   collection << yield(array[i])
+   collection.upcase << yield(array.[i])
    i += 1 
  end
- array.upcase
+ array
 end
 
